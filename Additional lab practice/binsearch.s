2e4403a20e56d6609f429cfe1e73f42a9d48fa4e
bogdanvladmihai <v.bogdan19@yahoo.com>
2022-11-07 22:30:37 +0200

    Binary search

--- Additional lab practice/binsearch.s
+++ Additional lab practice/binsearch.s
@@ -3,7 +3,7 @@
     len: .long 6
     l: .long 0
     r: .long 5
-    x: .long 7777
+    x: .long 7
     b: .long 2
     nf: .asciz "Not found\n"
     fd: .asciz "Found\n"
@@ -23,7 +23,8 @@ bin:
     mov $0, %edx
     idivl b
     movl %eax, %ecx
-    mov (%edi, %ecx, 4), %eax
+    movl %eax, %edx
+    movl (%edi, %ecx, 4), %eax
     cmp %eax, x
     je pass
     cmp %eax, x
@@ -31,12 +32,12 @@ bin:
     jmp lUp
 
 lUp:
-    movl %eax, l
+    movl %edx, l
     addl $1, l
     jmp bin
 
 rDown:
-    movl %eax, r
+    movl %edx, r
     subl $1, r
     jmp bin
 
@@ -44,7 +45,7 @@ fail:
     mov $4, %eax
     mov $1, %ebx
     mov $nf, %ecx
-    mov $15, %edx
+    mov $11, %edx
     int $0x80
     jmp exit
 
